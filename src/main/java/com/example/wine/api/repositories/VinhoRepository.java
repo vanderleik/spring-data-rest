6485de0df0840236b42a5c403102ce2a87b48242
vanderleik <vanderleik@yahoo.com.br>
2023-11-01 20:44:00 -0300

    feat: adicionando @RepositoryRestResource

--- src/main/java/com/example/wine/api/repositories/VinhoRepository.java
+++ src/main/java/com/example/wine/api/repositories/VinhoRepository.java
@@ -2,6 +2,8 @@ package com.example.wine.api.repositories;
 
 import com.example.wine.api.model.Vinho;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "vinhos", path = "vinhos")
 public interface VinhoRepository extends JpaRepository<Vinho, Long> {
 }
