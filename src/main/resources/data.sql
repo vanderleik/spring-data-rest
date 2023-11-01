2925ccfd2d036355c11ea7c30a29fe88134594cc
vanderleik <vanderleik@yahoo.com.br>
2023-11-01 20:06:17 -0300

    feat: adicionando dados no BD H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into vinhos (id,nome,uvas,produtor,regiao,pais,tipo,teorAlcoolico,safra,harmonizacao)
+values (1,'M. Chapoutier Côtes du Roussillon AOP','Grenache Blanc, Grenache Gris, Macabeo','M. Chapoutier','Languedoc-Roussillon','França','BRANCO',13.0,2021,'Carnes brancas, Frutos do mar, Queijos, Risoto e massas de molho branco, Saladas e aperitivos'),
+(2,'Tinto de Castelão by António Maçanita Alentejo DOC','Castelão','António Maçanita','Alentejo','Portugal','TINTO', 14.0,2019,	'Carnes vermelhas, Queijos'),
+(3,'Flor de Sal III Silver Antão Vaz Chardonnay Vinho Regional Alentejano','Chardonnay','Antão Vaz	Ervideira', 'Alentejo', 'Portugal', 'BRANCO',13.0,2020, 'Frutos do mar, Queijos, Saladas e aperitivos'),
+(4,'Alto De La Ballena Tannat-Merlot-Cabernet Franc','Merlot, Tannat, Cabernet Franc', 'Alto De La Ballena', 'Maldonado', 'Uruguai', 'TINTO', 13.5, 2016, 'Carnes vermelhas, Pizzas e massas de molho vermelho, Queijos'),
+(5,'Marchesi del Salento Pinot Grigio Blush Terre Siciliane IGT','Pinot Grigio','Castellani', 'Sicília','Itália','ROSE',12.0,2022, 'Carnes brancas, Frutos do mar, Queijos, Saladas e aperitivos'),
+(6,'Luigi Bosca Malbec DOC De Sangre', 'Malbec', 'Luigi Bosca', 'Lujan Cuyo, Medoza', 'Argentina', 'TINTO', 14.5, 2020,	'Parrillada, carnes vermelhas assadas na brasa, carré de cordeiro com cogumelos salteados'),
+(7,'Bella Conchi Cava Brut Rosé', 'Trepat', 'Bella Conchi', 'Penedés, Catalunya', 'Espanha', 'FRISANTES_ESPUMANTES',11.5, NULL, 'Perfeito como aperitivo, tapas espanholas variadas, badejo, queijos');
